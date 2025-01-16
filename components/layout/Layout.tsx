3d6854c8b2f67ad9f37e7ad5b61ba6748783fb3a
IlyaGoncharovY <goncharov.i@rambler.ru>
2025-01-16 21:28:42 +0300

    feat(layout) added Layout.tsx wrapper for our app.

---
+++ components/layout/Layout.tsx
@@ -0,0 +1,17 @@
+import React, {PropsWithChildren, ReactElement} from 'react';
+import {NextPage} from "next";
+import {Navbar} from "@/components/navbar/Navbar";
+import styles from "@/styles/Home.module.css";
+
+export const Layout: NextPage<PropsWithChildren> = ({children}) => {
+    return (
+        <main className={styles.main}>
+            <Navbar/>
+            {children}
+        </main>
+    );
+};
+
+export const getLayout = (page: ReactElement) => {
+    return <Layout>{page}</Layout>
+};
