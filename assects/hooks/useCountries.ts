4e9cc23c5bc2ab9a70967e967eca9b786b532859
IlyaGoncharovY <goncharov.i@rambler.ru>
2025-03-24 14:44:41 +0300

    fix() security update for next

--- assects/hooks/useCountries.ts
+++ assects/hooks/useCountries.ts
@@ -1,34 +1,34 @@
-import {useEffect, useState} from "react";
+import {useEffect, useState} from 'react';
 
 export interface ICountry {
     flag_url : string,
-    "name_ru" : string,
-    "iso_code2" : string,
-    "iso_code3" : string
+    'name_ru' : string,
+    'iso_code2' : string,
+    'iso_code3' : string
 }
 
 export const useCountries = (fetchCountries: () => Promise<ICountry[]>) => {
-    const [countryList, setCountryList] = useState<ICountry[]>([]);
+  const [countryList, setCountryList] = useState<ICountry[]>([]);
 
-    const removeCountry = (code: string) => {
-        setCountryList((prev) => prev.filter((country) => country.iso_code2 !== code));
+  const removeCountry = (code: string) => {
+    setCountryList((prev) => prev.filter((country) => country.iso_code2 !== code));
+  };
+
+  useEffect(() => {
+    const loadCountries = async () => {
+      try {
+        const countries = await fetchCountries();
+        setCountryList(countries);
+      } catch (error) {
+        console.error('Error fetching countries:', error);
+      }
     };
 
-    useEffect(() => {
-        const loadCountries = async () => {
-            try {
-                const countries = await fetchCountries();
-                setCountryList(countries);
-            } catch (error) {
-                console.error('Error fetching countries:', error);
-            }
-        };
+    loadCountries();
+  }, [fetchCountries]);
 
-        loadCountries();
-    }, [fetchCountries]);
-
-    return {
-        countryList,
-        removeCountry,
-    }
+  return {
+    countryList,
+    removeCountry,
+  };
 };
